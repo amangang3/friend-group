8ff5dc3b3b22fc24a5ea62832cf24d2f88dc6b96
amangang3 <aman.ganglani.16@ucl.ac.uk>
2019-10-23 10:05:02 +0100

    my version of friend group

--- group.py
+++ group.py
@@ -1,5 +1,48 @@
-"""An example of how to represent a group of acquaintances in Python."""
+#!/usr/bin/env python
+# coding: utf-8
 
-# Your code to go here...
+# In[3]:
 
-my_group =
+
+Jill = {"name" : "Jill",
+       "age" : 26, 
+       "job" : "biologist",
+       "relationships" : {
+           "friend" : "Zalika",
+           "partner" : "John"}
+       }
+
+Zalika = {"Name" : "Zalika",
+         "age" : 28, 
+         "job" : "artist",
+         "relationships" : {
+             "friend" : "Jill"}
+       }
+
+John = {"name" : "John",    
+       "age" : 27, 
+       "job" : "writer",
+       "relationships" : {
+           "partner" : "Jill"}
+       }
+
+Nash = {"name" : "Nash",
+       "age" : 34, 
+       "job" : "chef",
+       "relationships" : {
+           "cousin" : "John",
+           "landlord" : "Zalika"}
+       }
+
+my_group = [Jill, Zalika, John, Nash]
+
+print(my_group)
+
+ 
+
+
+# In[ ]:
+
+
+
+
